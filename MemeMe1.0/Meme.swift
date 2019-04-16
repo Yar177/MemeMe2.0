b83826c19ca55efb50da572ce4d2a6096b17300f
Yar Sher <yarser.dev@gmail.com>
2019-04-16 14:51:48 -0400

    create Meme

---
+++ MemeMe1.0/Meme.swift
@@ -0,0 +1,19 @@
+//
+//  Meme.swift
+//  MemeMe1.0
+//
+//  Created by Yar Sher on 4/16/19.
+//  Copyright © 2019 Yar Sher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+    
+}
+
